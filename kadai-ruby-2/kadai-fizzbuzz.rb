cc44130abef2c1b78f612acf5c68eca3dc6099ab
kazuya.haga <hg.future.160@icloud.com>
2021-08-12 11:23:18 +0000

    first

--- kadai-ruby-2/kadai-fizzbuzz.rb
+++ kadai-ruby-2/kadai-fizzbuzz.rb
@@ -1,11 +1,11 @@
 def FizzBuzz(num)
   
-  if num % 3 == 0
-    puts 'Fizz'
+  if num % 15 == 0
+    puts 'FizzBuzz'
   elsif num % 5 == 0
     puts 'Buzz'
-  elsif num % 15 == 0
-    puts 'FizzBuzz'
+  elsif num % 3 == 0
+    puts 'Fizz'
   else 
     puts num
   end
